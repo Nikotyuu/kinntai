6a82791742f2cb1f5d61080245c0278a6ac544dc
Nikotyuu <nikotyuu0103@email.com>
2020-02-08 14:30:37 +0000

    Update Gemfile for Heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,11 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: [:index,:destroy, :edit_basic_info, :update_basic_info]
+  before_action :admin_or_correct_user, only: [:show]
   before_action :set_one_month, only: :show
 
   def index
-    @users = User.paginate(page: params[:page])
+      @users = User.paginate(page: params[:page]).search(params[:search])
   end
 
   def show
@@ -58,6 +59,17 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
+   def will_paginate
+   end
+   
+   def admin_or_correct_user
+      @user = User.find(params[:user_id]) if @user.blank?
+      unless current_user?(@user) || current_user.admin?
+        flash[:danger] = "編集期限がありません。" 
+        redirect_to(root_url)
+      end
+   end
+
   private
 
     def user_params
@@ -67,4 +79,5 @@ class UsersController < ApplicationController
     def basic_info_params
       params.require(:user).permit(:department, :basic_time, :work_time)
     end
+    
 end
