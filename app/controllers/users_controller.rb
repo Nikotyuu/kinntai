556c6a47803e4c7036a1e4bebd7c041f25315e85
Nikotyuu <ec2-user@ip-172-31-39-71.ap-northeast-1.compute.internal>
2020-03-05 13:55:31 +0000

    19

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -71,6 +71,16 @@ class UsersController < ApplicationController
       end
    end
    
+   def import
+    if params[:csv_file].blank?
+      redirect_to(users_url, alert: 'インポートするcsvファイルを選択してください')
+    else
+      num = Admin::User.import(params[:csv_file])
+      redirect_to(users_url, notice: "#{num.to_s}件のユーザー情報を追加/更新しました")
+    end
+    
+   end
+   
 
   private
 
